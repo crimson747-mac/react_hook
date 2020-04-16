909383e8a9feb9749e3a46f3a9f14c4c9531534c
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-04-17 01:40:16 +0900

    useInput part Two

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { useState } from "react";
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [vlaue, setValue] = useState(initialValue);
   const onChange = (event) => {
-    console.log(event.target);
+    const {
+      target: { value },
+    } = event;
+
+    let willUpdate = true;
+
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { vlaue, onChange };
 };
 
 const App = () => {
-  const name = useInput("Mr.");
+  const maxLength = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLength);
+
   return (
     <div className="App">
       <h1>Hello</h1>
