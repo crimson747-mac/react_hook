4d12d9bf35d91b40a9ba66d9b674b7ac3d309be9
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-04-17 01:59:51 +0900

    useTabs

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
 import React, { useState } from "react";
 
-const useInput = (initialValue, validator) => {
-  const [vlaue, setValue] = useState(initialValue);
-  const onChange = (event) => {
-    const {
-      target: { value },
-    } = event;
+const content = [
+  {
+    tab: "Section 1",
+    content: "I'm the content of the Section 1",
+  },
+  {
+    tab: "Section 2",
+    content: "I'm the content of the Section 2",
+  },
+];
 
-    let willUpdate = true;
-
-    if (typeof validator === "function") {
-      willUpdate = validator(value);
-    }
-
-    if (willUpdate) {
-      setValue(value);
-    }
+const useTabs = (initialTab, allTabs) => {
+  // if (!allTabs || !Array.isArray(allTabs)) {
+  //   return;
+  // }
+  const [currentIndex, setCurrentIndex] = useState(initialTab);
+  return {
+    currentItem: allTabs[currentIndex],
+    changeItem: setCurrentIndex,
   };
-  return { vlaue, onChange };
 };
 
 const App = () => {
-  const maxLength = (value) => value.length <= 10;
-  const name = useInput("Mr.", maxLength);
-
+  const { currentItem, changeItem } = useTabs(0, content);
   return (
     <div className="App">
       <h1>Hello</h1>
-      <input placeholder="Name" onChange={name.onChange} value={name.vlaue} />
-      {/* <input placeholder="Name" {...name} /> */}
+      {content.map((section, index) => (
+        <button key={index} onClick={() => changeItem(index)}>
+          {section.tab}
+        </button>
+      ))}
+      <div>{currentItem.content}</div>
     </div>
   );
 };
