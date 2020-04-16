c34886de007db2a274ad927c523793d7d17fdd41
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-04-17 01:28:48 +0900

    useInput

--- src/App.js
+++ src/App.js
@@ -1,48 +1,22 @@
 import React, { useState } from "react";
 
+const useInput = (initialValue) => {
+  const [vlaue, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    console.log(event.target);
+  };
+  return { vlaue, onChange };
+};
+
 const App = () => {
-  // useState()는 초기값(여기서는 1) 과 이 값을 변형할 수 있는 두번째 인자(함수) 를 배열의 형태로 리턴한다.
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const name = useInput("Mr.");
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <button onClick={incrementItem}>incrementItem</button>
-      <button onClick={decrementItem}>decrementItem</button>
+      <h1>Hello</h1>
+      <input placeholder="Name" onChange={name.onChange} value={name.vlaue} />
+      {/* <input placeholder="Name" {...name} /> */}
     </div>
   );
 };
-
-class AppUgly extends React.Component {
-  state = {
-    item: 1,
-  };
-
-  incrementItem = () => {
-    this.setState({
-      item: this.state.item + 1,
-    });
-  };
-
-  decrementItem = () => {
-    this.setState({
-      item: this.state.item - 1,
-    });
-  };
-
-  render() {
-    const { item } = this.state;
-    return (
-      <div className="App">
-        <h1>Hello! {item}</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <button onClick={this.incrementItem}>incrementItem</button>
-        <button onClick={this.decrementItem}>decrementItem</button>
-      </div>
-    );
-  }
-}
 
 export default App;
