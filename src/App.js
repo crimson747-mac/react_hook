455d860b196b7e0dcd25e675e26b42a358a1ccf5
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-04-18 00:31:36 +0900

    useConfirm

--- src/App.js
+++ src/App.js
@@ -1,34 +1,28 @@
 import React, { useState, useEffect, useRef } from "react";
 
-//useRef() - document.getElementById() 와 같이 DOM 요소를 참조할 수 있다.
-//JSX 는 ref 라는 것을 가지고 이를 이용할 수 있다.
-//useRef() 로 가져온 값은 default로 current 안에 DOM 요소를 가진다.
+const useConfirm = (message = "", callback, rejection) => {
+  if (callback && typeof callback !== "function") {
+    return;
+  }
 
-const useClick = (onClick) => {
-  const element = useRef();
-
-  useEffect(() => {
-    if (element.current) {
-      element.current.addEventListener("click", onClick);
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      callback();
+    } else {
+      rejection();
     }
+  };
 
-    //useEffect에서 함수를 return 하면 그 함수는 componentWillUnMount시에 호출이 된다.
-    return () => {
-      if (element.current) {
-        element.current.removeEventListener("click", onClick);
-      }
-    };
-  }, []);
-  return element;
+  return confirmAction;
 };
 
 const App = () => {
-  const sayHello = () => console.log("say hello");
-  const title = useClick(sayHello);
-
+  const deleteWord = () => console.log("Deleting the word");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure", deleteWord, abort);
   return (
     <div className="App">
-      <h1 ref={title}>Hello</h1>
+      <button onClick={confirmDelete}>Delete the word</button>
     </div>
   );
 };
