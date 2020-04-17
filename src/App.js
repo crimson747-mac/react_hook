4cc7a3aa26f4b823035c6c23c3e1992b2c95d600
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-04-17 09:50:03 +0900

    useTitle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { useState, useEffect } from "react";
 
-// useEffect: componentWillUnmount + componentDidMount + componentDidUpdate
-// useEffect 는 첫 번째 인자로 함수를 받는다.
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updatetitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerHTML = title;
+  };
+  useEffect(updatetitle, [title]);
+  return setTitle;
+};
 
-// 두 번째 인자는 deps(dependency) 이며 만약 deps가 있다면
-// useEffect는 deps 리스트에 있는 값일 때만 componentDidUpate가 발생한다.
-
-// useEffect는 함수를 리턴하는데 그게 componentWillUnmount 이다.
 const App = () => {
-  const sayHello = () => console.log("hello");
-
-  const [number, setNumber] = useState(0);
-  const [aNumber, setAnumber] = useState(0);
-  useEffect(sayHello, [number]);
-  //useEffect(sayHello, []); => compoenentDidMount 가 로드시 한 번만 실행된다.
-
+  const titleHandler = useTitle("Loading...");
+  setTimeout(() => titleHandler("Home"), 3000);
   return (
     <div className="App">
       <h1>Hello</h1>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 };
