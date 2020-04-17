3dba2e0a3ff6943adb51a82ada2ca22a4ba04984
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-04-18 00:16:32 +0900

    useClick

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-const useTitle = (initialTitle) => {
-  const [title, setTitle] = useState(initialTitle);
-  const updatetitle = () => {
-    const htmlTitle = document.querySelector("title");
-    htmlTitle.innerHTML = title;
-  };
-  useEffect(updatetitle, [title]);
-  return setTitle;
+//useRef() - document.getElementById() 와 같이 DOM 요소를 참조할 수 있다.
+//JSX 는 ref 라는 것을 가지고 이를 이용할 수 있다.
+//useRef() 로 가져온 값은 default로 current 안에 DOM 요소를 가진다.
+
+const useClick = (onClick) => {
+  const element = useRef();
+
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+
+    //useEffect에서 함수를 return 하면 그 함수는 componentWillUnMount시에 호출이 된다.
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
 };
 
 const App = () => {
-  const titleHandler = useTitle("Loading...");
-  setTimeout(() => titleHandler("Home"), 3000);
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 ref={title}>Hello</h1>
     </div>
   );
 };
