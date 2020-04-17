1282bd224fccb4bd6a5d173393b75b59ea91968e
crimson747-mac <58840453+crimson747-mac@users.noreply.github.com>
2020-04-18 00:48:27 +0900

    usePreventLeave

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import React, { useState, useEffect, useRef } from "react";
 
-const useConfirm = (message = "", callback, rejection) => {
-  if (callback && typeof callback !== "function") {
-    return;
-  }
-
-  const confirmAction = () => {
-    if (window.confirm(message)) {
-      callback();
-    } else {
-      rejection();
-    }
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = ""; // google chrome only
   };
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
 
-  return confirmAction;
+  return { enablePrevent, disablePrevent };
 };
 
 const App = () => {
-  const deleteWord = () => console.log("Deleting the word");
-  const abort = () => console.log("Aborted");
-  const confirmDelete = useConfirm("Are you sure", deleteWord, abort);
+  const { enablePrevent, disablePrevent } = usePreventLeave();
   return (
     <div className="App">
-      <button onClick={confirmDelete}>Delete the word</button>
+      <button onClick={enablePrevent}>protect</button>
+      <button onClick={disablePrevent}>unprotect</button>
     </div>
   );
 };
